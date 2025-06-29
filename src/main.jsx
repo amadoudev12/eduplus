902e560495460e0f34c6f3f528123d763796bee0
amadoudev12 <delhadjamadou670@gmail.com>
2025-06-29 19:13:32 +0100

    mod

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+// import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <HashRouter>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    </HashRouter>
+  </StrictMode>,
 )
+
